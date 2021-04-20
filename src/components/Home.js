725997f17edef455b558a829506d69d408bf3b8c
Alyssa <Alyssa.Ma80@myhunter.cuny.edu>
2021-04-19 23:40:29 -0400

    changed bank img

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank" />
+                <img src="https://photos2.fotosearch.com/bthumb/CSP/CSP996/bank-icon-clipart__k17206634.jpg" alt="bank" />
                 <h1>Bank of React</h1>
 
                 <div>
