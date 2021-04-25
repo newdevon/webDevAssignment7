c1c1feffede023a3e0c28810d4c5d7c55273d68a
Steven C <stvchan999@gmail.com>
2021-04-25 18:07:14 -0400

    styling for Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
+import './Login.css'
 
 class LogIn extends Component {
   constructor () {
@@ -34,15 +35,16 @@ class LogIn extends Component {
     }
 
     return (
-      <div>
+      <div className='center'>
         <form onSubmit={this.handleSubmit}>
           <div>
-            <label htmlFor="userName">User Name</label>
+            <h1>Sign-in</h1>
+            <label htmlFor="userName">User Name: </label>
             <input type="text" name="userName" onChange={this.handleChange} value={this.state.user.userName} />
           </div>
           <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" />
+            <label htmlFor="password">Password: </label>
+            <input className="passName" type="password" name="password" />
           </div>
           <button>Log In</button>
         </form>
